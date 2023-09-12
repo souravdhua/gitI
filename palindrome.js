2c4a20ac7067169ef27624cbab18b1bba8cada7f
Sourav Dhua <souravdhua1997@gmail.com>
2023-09-13 03:47:09 +0530

    added palindrome code

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+let str = "oyo" ;
+let rev = "" ;
+for(let i = str.length - 1 ; i >= 0 ; i--){
+	rev += str[i];
+	}
+if(rev == str){
+	console.log("Palindrome);
+}else{
+	console.log("Not Palindrome);
+}
